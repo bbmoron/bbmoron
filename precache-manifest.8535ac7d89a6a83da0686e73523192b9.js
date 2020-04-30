2117c5bd05c6481c7f8f855e6d35e869efa3988e
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2020-04-30 19:03:37 +0300

    Updates

---
+++ precache-manifest.8535ac7d89a6a83da0686e73523192b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344adcc45015a8544ea1f1e5543f4424",
+    "url": "/bbmrn/index.html"
+  },
+  {
+    "revision": "8d226c10128cf849cd88",
+    "url": "/bbmrn/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5974443812aa884a7330",
+    "url": "/bbmrn/static/js/2.ce67451d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bbmrn/static/js/2.ce67451d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d226c10128cf849cd88",
+    "url": "/bbmrn/static/js/main.67b6606c.chunk.js"
+  },
+  {
+    "revision": "959c4816cbe63a2e8fdb",
+    "url": "/bbmrn/static/js/runtime-main.e56df4a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bbmrn/static/media/logo.5d5d9eef.svg"
+  }
+]);
